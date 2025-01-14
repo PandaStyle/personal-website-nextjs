a59dc5c571ca45dd991a6990431f8350dda03e27
Zsolt Nemeth <furion1@gmail.com>
2025-01-14 13:42:28 +0100

    fix app type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import type { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
 import '@/styles/globals.css';
 import '@/styles/styles.scss';
@@ -8,7 +9,7 @@ import Navbar from '@/components/Navbar';
 // If loading a variable font, you don't need to specify the font weight
 
 
-export default function App({ Component, pageProps, router }: { Component: React.ComponentType<any>; pageProps: any; router: any }) {
+export default function App({ Component, pageProps, router }: AppProps) {
     return (
         <div className='main'>
             <Navbar />
